13bed5cd73259f1e9b0b59c2681297e3b9d89489
李瑞 <474640211@qq.com>
2017-03-07 00:14:44 +0800

    update

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,19 +1,20 @@
 <template lang="html">
+  <div class="ratings-wapper">
       <div class="ratings">
             <div class="ratings-header">
                   <div class="ratings-header-appraise">
                         <h2>3.9</h2>
                         <p>综合评分</p>
-                        <span>高于周边商家{{good.rankRate}}</span>
+                        <span>高于周边商家{{good.rankRate}}%</span>
                   </div>
-                  <div class="ratings-header-grade">
+                  <div class="ratings-header-zone">
                         <div class="grade-start">
                               <span>服务态度</span>
-                              <start></start>
+                              <star :size="36" :score="good.serviceScore"></star>
                         </div>
                         <div class="grade-start">
                               <span>服务态度</span>
-                              <start></start>
+                              <star :size="36" :score="good.foodScore"></star>
                         </div>
                         <div class="grade-start">
                               <span>送达时间</span>
@@ -24,11 +25,13 @@
             <ratingselect></ratingselect>
             <!--数据展示 -->
       </div>
+      </div>
 </template>
 
 <script>
       // 引入 star 组件
       import star from '../star/star';
+      import BScorll from 'better-scroll';
       import ratingselect from 'components/ratingselect/ratingselect.vue'
       const ERR_OK = 0;
       export default {
@@ -51,21 +54,29 @@
                         console.log(this.good.name);
                   });
             }
-
-
       }
 </script>
 
 <style lang="scss">
+
     .ratings{
+      &-wapper{
+            position:absolute;
+            top:174px;
+            left:0;
+            bottom:0;
+            width:100%;
+            overflow:hidden;
+      }
       &-header{
             display:flex;
-            justify-content: space-between;
       &-appraise{
              width:138px;
-             padding:18px 0;
              text-align:center;
              position:relative;
+             margin:18px 0;
+             border-right:1px solid #eaeaea;
+             flex:0 0 138px;
               h2{
               font-size:24px;
               color:rgb(255,153,0);
@@ -84,6 +95,27 @@
               }
               
             } 
+      }
+      .ratings-header-zone{
+            padding:18px 24px;
+            flex:1;
+           .grade-start{
+                 display:flex;
+                 justify-content: flex-start;
+                  >span{
+               display:inline-block;
+               font-size:12px;
+               color:rgb(7,17,27);
+               margin:0 12px 8px 0;
+               line-height: 18px;
+                   }
+            >time{
+                  display:inline-block;
+                  vertical-align:middle;
+                  font-size:12px;
+                  color:rgb(147,153,159);
+            }
+           }
       }
     }
 </style>
