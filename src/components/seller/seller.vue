d08ad32229394c909c340936f89fc2be792f89e6
liruihaod <474640211@qq.com>
2017-03-08 17:54:46 +0800

    asdasd

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,10 +1,50 @@
 <template lang="html">
-    <div>I am 123123123</div>
+    <div class="seller">
+        <div class="seller-wrapper">
+            <div class="seller-wrapper-header">
+                <div>
+                    <div class="seller-left">
+                        <h2>{{seller.name}}</h2>
+                        <div>
+                            <star :size="48" :score="seller.score"></star>
+                            <span>({{seller.ratingCount}}份)</span>
+                            <span>月售{{seller.sellCount}}</span> 
+                        </div>
+                    </div>
+                    <div class="seller-right">
+                        <span class="icon-favorite"></span>
+                        <span class="selelr-right-tips">
+                            已收藏
+                        </span>
+                    </div> 
+                </div>
+            </div>
+            <div class="seller-warpper-nav">
+                <div>
+                    <p>起送价</p>
+                    <span>{{seller.minPrice}}元</span>
+                </div>
+                <div>
+                    <p>商家配送</p>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-}
+    import star from '../star/star';
+    import BScroll from 'better-scroll';
+    export default {
+        props: {
+            seller: {
+                type: Object
+            }
+        }, components: {
+            star
+        },
+    }
 </script>
 
 <style lang="scss">
